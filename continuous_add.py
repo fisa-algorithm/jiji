1c5731733c73308e41c1b53011540e30a707840c
Jiyoung July <31668639+JiJuOh@users.noreply.github.com>
2023-12-19 19:32:58 +0900

    리팩토링 완료

--- continuous_add.py
+++ continuous_add.py
@@ -17,11 +17,30 @@ def prac(a,b):
     print(big)
 
 
+prac("10", "10 -4 3 1 5 6 -35 12 21 -1") #33
+prac("10","2 1 -4 3 4 -4 6 5 -5 1") # 14
+prac("5","-1 -2 -3 -4 -5") #-1
 
 
+##################### 38828KB	60ms #####################
 
+def prac():
+    total = inp[0]
+    big = inp[0]
+    for x in inp[1:]:
+        if total + x > x:
+            total += x
+        else:
+            total = x
+        if (total > big):
+            big = total
+    return big
 
+a = input()
+inp = list(map(int, input().split()))
+print(prac())
 
-prac("10", "10 -4 3 1 5 6 -35 12 21 -1")
-prac("10","2 1 -4 3 4 -4 6 5 -5 1")
-prac("5","-1 -2 -3 -4 -5")
+'''
+1. 전역변수 이용하고 함수에 prameter로 넘겨주지 않는다
+2. map을 forloop 대신에 사용하기
+'''
